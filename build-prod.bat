e9817ef57ccd39e3831e3536fb5baa9bee99e7f1
Yan Pujante <yan@pongasoft.com>
2018-08-22 11:22:11 -0700

    Fixes for Windows 10

--- build-prod.bat
+++ build-prod.bat
@@ -8,9 +8,39 @@ Echo SRC_DIR=%SRC_DIR%
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-cmake -G"Visual Studio 15 2017 Win64" -DCMAKE_CXX_FLAGS=/D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING %SRC_DIR%
+Echo =============================================================
+Echo ==                                                         ==
+Echo ==              Generating Makefiles...                    ==
+Echo ==              -----------------------                    ==
+Echo =============================================================
+cmake -G"Visual Studio 15 2017 Win64" %SRC_DIR%
+
+REM Compiling tests
 cmake --build . --config Release --target pongasoft_JambaSampleGain_test
-cmake --build . --config Release --target jamba_test
+
+Echo =============================================================
+Echo ==                                                         ==
+Echo ==              Running Tests..........                    ==
+Echo ==              -----------------------                    ==
+Echo =============================================================
 ctest -C Release
 
+REM Compiling Plugin
+cmake --build . --config Release --target pongasoft_JambaSampleGain 
+
+REM Compiling validator
+cmake --build . --config Release --target validator
+
+Echo =============================================================
+Echo ==                                                         ==
+Echo ==              Validating Plugin......                    ==
+Echo ==              -----------------------                    ==
+Echo =============================================================
+.\bin\Release\validator.exe VST3\Release\pongasoft_JambaSampleGain.vst3 
+
+Echo =============================================================
+Echo ==                                                         ==
+Echo ==              Building archive.......                    ==
+Echo ==              -----------------------                    ==
+Echo =============================================================
 cmake --build . --config Release --target archive
